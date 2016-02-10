e9274bd76a47b8b2848edfd066d90d66ef2a480a
Amelia Kaufman <amelia.kaufman@gmail.com>
2016-02-10 15:30:42 -0500

    updated schema with invitee migration

--- whrsthat/db/schema.rb
+++ whrsthat/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160210200029) do
+ActiveRecord::Schema.define(version: 20160210200928) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,12 @@ ActiveRecord::Schema.define(version: 20160210200029) do
     t.integer  "lat"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
+  end
+
+  create_table "invitees", force: :cascade do |t|
+    t.boolean  "attending"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
