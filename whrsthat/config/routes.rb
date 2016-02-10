c1fff9fef20ad518a8e854ac94fe59fd3cd53d0e
Josue Cardona <josuecardonap@gmail.com>
2016-02-10 15:29:02 -0500

    Removed repeated invitees resources from routes.rb

--- whrsthat/config/routes.rb
+++ whrsthat/config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  resources :invitees
-  resources :invitees
-  resources :invitees
-  resources :invitees
+  resources :invitees 
   resources :events
   resources :users
   # The priority is based upon order of creation: first created -> highest priority.
