ac221a2af265681837c31313cac363e72cdce2b2
Zachary Boyd <zachary@zacharyboyd.nyc>
2016-02-13 09:33:59 -0500

    restyle event page

--- whrsthat/app/controllers/events_controller.rb
+++ whrsthat/app/controllers/events_controller.rb
@@ -40,21 +40,6 @@ class EventsController < ApplicationController
       if @event.save
 
         # file = File.open('./public/IMG_3503.JPG')
-        photo = EXIFR::JPEG.new(tempfile.path)
-        
-        google_server_key = ENV["GOOGLE_SERVER_KEY"];
-        lat = photo.exif[0].gps_latitude[0].to_f + (photo.exif[0].gps_latitude[1].to_f / 60) + (photo.exif[0].gps_latitude[2].to_f / 3600)
-        long = photo.exif[0].gps_longitude[0].to_f + (photo.exif[0].gps_longitude[1].to_f / 60) + (photo.exif[0].gps_longitude[2].to_f / 3600)
-        long = long * -1 if photo.exif[0].gps_longitude_ref == "W"   # (W is -, E is +)
-        lat = lat * -1 if photo.exif[0].gps_latitude_ref == "S"      # (N is +, S is -)
-
-        lat_to_string = lat.to_s
-        long_to_string = long.to_s
-
-        google_uri = URI("https://maps.googleapis.com/maps/api/geocode/json?latlng=#{lat_to_string},#{long_to_string}&key=#{google_server_key}")
-        result = Net::HTTP.get(google_uri)
-        photo_data = JSON.parse(result)
-        @@address = photo_data.flatten[1][0]["formatted_address"] # save to db
 
         format.html { redirect_to @event, notice: 'Event was successfully created.' }
         format.json { render :show, status: :created, location: @event }
